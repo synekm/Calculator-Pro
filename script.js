4da23d34c62a985814a6810c84ee1a2ac8e32426
Martin Synek <synekm@spstrutnov.cz>
2020-09-29 13:05:33 +0200

    funkce cislo() a aktualizacePrikladu()

---
+++ script.js
@@ -0,0 +1,66 @@
+var cislice = "";
+
+function plus() {
+
+}
+
+function minus() {
+
+}
+
+function deleni() {
+
+}
+
+function nasobeni() {
+
+}
+
+function jednaDelenoX() {
+
+}
+
+function naDruhou() {
+
+}
+
+function odmocnina() {
+
+}
+
+function procento() {
+
+}
+
+function c() {
+
+}
+
+function ce() {
+
+}
+
+function del() {
+
+}
+
+function plusLomenoMinus() {
+
+}
+
+function carka() {
+
+}
+
+function vysledek() {
+
+}
+
+function cislo(a) {
+    cislice += a.innerHTML;
+    console.log(cislice);
+}
+
+function aktualizacePrikladu() {
+    document.getElementById("priklad").value = cislice;
+}
