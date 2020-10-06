53cd1d9e4ae8105df65e2130a38fdf9c60fac3ae
Martin Synek <synekm@spstrutnov.cz>
2020-10-06 13:21:12 +0200

    Vymazání fce aktualizacePrikladu() a předělání operátora a čísla.

--- script.js
+++ script.js
@@ -2,30 +2,26 @@ var cislo = "";
 var predtimCislo = "";
 var jeOperator = false;
 var operace = "";
+var staraOperace = "";
 var pocetOperaci = 0;
 
 function ziskatCislo(cislice) {
     cislo += cislice.innerHTML;
     parseFloat(cislo);
-    console.log(cislo);
+    document.getElementById("priklad").value = cislo;
 }
 
 function ziskatOperator(operator) {
     operace = operator.innerHTML;
     jeOperator = true;
-    console.log(operace);
-}
-
-function aktualizacePrikladu() {
-    document.getElementById("priklad").value = cislice;
-    if (jeOperator == true) {
+    
+    if (predtimCislo == "") {
         predtimCislo = cislo;
         cislo = "";
-
-        jeOperator = false;
     }
 
-    if (cislo != "" && predtimCislo != "") {
+    document.getElementById("priklad").value = "";
 
-    }
+    console.log("predchozi " + predtimCislo);
+    console.log("operator " + operace);
 }
