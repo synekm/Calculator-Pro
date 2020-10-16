9ef7e532d13a612d7e1430c051c128320d00f974
Martin Synek <synekm@spstrutnov.cz>
2020-10-16 19:19:01 +0200

    fce vymazat a naDruhou + priklad se bude vypisovat z prava

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var priklad = "";
-var vysledek = "";
 
 function ziskatPriklad(cast) {
     priklad += cast.innerHTML;
     document.getElementById("priklad").value = priklad;
 }
 
-function ziskejVysledek() {
-    vysledek = eval(priklad);
-    document.getElementById("priklad").value = vysledek;
+function ziskatVysledek() {
+    priklad = eval(priklad);
+    document.getElementById("priklad").value = priklad;
+}
+
+function vymazat() {
+    priklad = "";
+    document.getElementById("priklad").value = priklad;
+}
+
+function naDruhou() {
+    priklad = priklad * priklad;
+    document.getElementById("priklad").value = priklad;
 }
