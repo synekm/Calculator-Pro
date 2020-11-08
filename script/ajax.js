0e4549cd67302bd26d9c1636e89a02811eb8ba89
Martin Synek <synekm@spstrutnov.cz>
2020-11-08 18:04:15 +0100

    Změna style.css, úprava ajax.js číslice, aby používala innerHTML, změna pozice onload.

--- script/ajax.js
+++ script/ajax.js
@@ -15,36 +15,35 @@ function nastavit() {
 }
 
 function odeslat(a) {
-    let cislice = 0;
+    let cislice = "";
 
-    if (a.value == 0 || a.value == 1 || a.value == 2 || a.value == 3 || a.value == 4 || a.value == 5 || a.value == 6 || a.value == 7 || a.value == 8 || a.value == 9) {
-        cislice = a.value;
+    if (a.innerHTML == 0 || a.innerHTML == 1 || a.innerHTML == 2 || a.innerHTML == 3 || a.innerHTML == 4 || a.innerHTML == 5 || a.innerHTML == 6 || a.innerHTML == 7 || a.innerHTML == 8 || a.innerHTML == 9) {
+        cislice = a.innerHTML;
     }
+
+    console.log(cislice);
 
     let xhr = new XMLHttpRequest();
 
     xhr.open("POST", "php/kalkulacka.php", true);
-
     xhr.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             let odpoved = JSON.parse(this.responseText);
 
             let html = "";
-            html += "<p>Cislice " + odpoved[1].cislice + " : " + odpoved[1].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[2].cislice + " : " + odpoved[2].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[3].cislice + " : " + odpoved[3].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[4].cislice + " : " + odpoved[4].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[5].cislice + " : " + odpoved[5].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[6].cislice + " : " + odpoved[6].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[7].cislice + " : " + odpoved[7].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[8].cislice + " : " + odpoved[8].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[9].cislice + " : " + odpoved[9].pocet + "</p>";
-            html += "<p>Cislice " + odpoved[10].cislice + " : " + odpoved[10].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[1].cislice + " : " + odpoved[1] + "</p>";
+            html += "<p>Číslice " + odpoved[2].cislice + " : " + odpoved[2].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[3].cislice + " : " + odpoved[3].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[4].cislice + " : " + odpoved[4].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[5].cislice + " : " + odpoved[5].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[6].cislice + " : " + odpoved[6].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[7].cislice + " : " + odpoved[7].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[8].cislice + " : " + odpoved[8].pocet + "</p>";
+            html += "<p>Číslice " + odpoved[9].cislice + " : " + odpoved[9].pocet + "</p>";
 
             _statistika.innerHTML = html;
         }
     };
-    
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("cislice=" + JSON.stringify(cislice));
 }
