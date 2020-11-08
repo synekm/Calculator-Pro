214aec4388d2d4fa653e36d7364366d1bdbe93aa
Martin Synek <synekm@spstrutnov.cz>
2020-11-08 15:41:57 +0100

    Přidání nového div, id a začátek předělání ajax.js.

--- php/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-$jmeno = $_GET["jmeno"];
-$heslo = $_POST["heslo"];
-
-$hash = password_hash($heslo, PASSWORD_DEFAULT);
-
-echo "<p>Jméno: ".$jmeno."</p>";
-echo "<p>Heslo: ".$heslo."</p>";
-echo "<p>Hash: ".$hash."</p>";
--- script/ajax.js
+++ script/ajax.js
@@ -1,37 +1,40 @@
-var _jmeno, _heslo, _formular;
+var _nula, _jedna, _dva, _tri, _ctyri, _pet, _sest, _sedm, _osm, _devet, _statistika;
 
 function nastavit() {
-    _jmeno = document.getElementById("jmeno");
-    _heslo = document.getElementById("heslo");
-    _formular = document.getElementById("formular");
+    _nula = document.getElementById("0");
+    _jedna = document.getElementById("1");
+    _dva = document.getElementById("2");
+    _tri = document.getElementById("3");
+    _ctyri = document.getElementById("4");
+    _pet = document.getElementById("5");
+    _sest = document.getElementById("6");
+    _sedm = document.getElementById("7");
+    _osm = document.getElementById("8");
+    _devet = document.getElementById("9");
+    _statistika = document.getElementById("statistika");
 }
 
-function odeslat() {
-    let data = {
-        jmeno: _jmeno.value,
-        heslo: _heslo.value
-    }
+function odeslat(a) {
+    let cislice
+
+    if (a.value == 0 || a.value == 1 || a.value == 2 || a.value == 3 || a.value == 4 || a.value == 5 || a.value == 6 || a.value == 7 || a.value == 8 || a.value == 9)
+        cislice = a.value;
 
     let xhr = new XMLHttpRequest();
 
-    xhr.open("POST", "php/ajax_json.php", true);
+    xhr.open("POST", "php/kalkulacka.php", true);
 
     xhr.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             let odpoved = JSON.parse(this.responseText);
 
-            _jmeno.value = "";
-            _heslo.value = "";
-
             let html = "";
-            html += "<p>Jméno (JSON): " + odpoved["jmeno"] + "</p>";
-            html += "<p>Heslo (JSON): " + odpoved["heslo"] + "</p>";
-            html += "<p>Hash (JSON): " + odpoved["hash"] + "</p>";
+            html += "<p>Cislice : " + odpoved["jmeno"] + "</p>";
 
-            _formular.innerHTML = html;
+            _statistika.innerHTML = html;
         }
     };
     
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("data=" + JSON.stringify(data));
+    xhr.send("cislice=" + JSON.stringify(cislice));
 }
