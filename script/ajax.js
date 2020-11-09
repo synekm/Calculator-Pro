ca48811e8e1cee839a1c1ad80c36402034a2912f
Martin Synek <synekm@spstrutnov.cz>
2020-11-09 22:12:20 +0100

    Změna POST na GET a funguje to!

--- script/ajax.js
+++ script/ajax.js
@@ -9,42 +9,65 @@ function odeslat(a) {
 
     if (a.innerHTML == 0 || a.innerHTML == 1 || a.innerHTML == 2 || a.innerHTML == 3 || a.innerHTML == 4 || a.innerHTML == 5 || a.innerHTML == 6 || a.innerHTML == 7 || a.innerHTML == 8 || a.innerHTML == 9) {
         cislice = a.innerHTML;
-    };
-    if (a.innerHTML == "CE" || a.innerHTML == "C") {
-        cislice = null;
-    };
 
-    console.log(cislice);
+        console.log(cislice);
 
-    let xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
-    xhr.open("POST", "php/kalkulacka.php", true);
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            let odpoved = JSON.parse(this.responseText);
+        xhr.open("GET", "php/kalkulacka.php?cislice=" + cislice, true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let odpoved = JSON.parse(this.responseText);
 
-            console.log(odpoved);
+                console.log(odpoved);
 
-            let html = "";
-            html += "<p>Číslice " + odpoved[0].cislice + " : " + odpoved[0].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[1].cislice + " : " + odpoved[1].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[2].cislice + " : " + odpoved[2].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[3].cislice + " : " + odpoved[3].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[4].cislice + " : " + odpoved[4].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[5].cislice + " : " + odpoved[5].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[6].cislice + " : " + odpoved[6].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[7].cislice + " : " + odpoved[7].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[8].cislice + " : " + odpoved[8].pocet + "</p>";
-            html += "<p>Číslice " + odpoved[9].cislice + " : " + odpoved[9].pocet + "</p>";
+                let html = "";
+                html += "<p>Číslice " + odpoved[0].cislice + " : " + odpoved[0].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[1].cislice + " : " + odpoved[1].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[2].cislice + " : " + odpoved[2].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[3].cislice + " : " + odpoved[3].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[4].cislice + " : " + odpoved[4].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[5].cislice + " : " + odpoved[5].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[6].cislice + " : " + odpoved[6].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[7].cislice + " : " + odpoved[7].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[8].cislice + " : " + odpoved[8].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[9].cislice + " : " + odpoved[9].pocet + "</p>";
 
-            _statistika.innerHTML = html;
+                _statistika.innerHTML = html;
+            };
         };
-    };
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    if (cislice != null) {
-        xhr.send("cislice=" + parseInt(cislice));
-    };
-    if (cislice == null) {
         xhr.send();
     };
+    if (a.innerHTML == "CE" || a.innerHTML == "C") {
+        cislice = undefined;
+
+        console.log(cislice);
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("GET", "php/kalkulacka.php", true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let odpoved = JSON.parse(this.responseText);
+
+                console.log(odpoved);
+
+                let html = "";
+                html += "<p>Číslice " + odpoved[0].cislice + " : " + odpoved[0].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[1].cislice + " : " + odpoved[1].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[2].cislice + " : " + odpoved[2].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[3].cislice + " : " + odpoved[3].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[4].cislice + " : " + odpoved[4].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[5].cislice + " : " + odpoved[5].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[6].cislice + " : " + odpoved[6].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[7].cislice + " : " + odpoved[7].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[8].cislice + " : " + odpoved[8].pocet + "</p>";
+                html += "<p>Číslice " + odpoved[9].cislice + " : " + odpoved[9].pocet + "</p>";
+
+                _statistika.innerHTML = html;
+            };
+        };
+        xhr.send();
+    };
+
 }
